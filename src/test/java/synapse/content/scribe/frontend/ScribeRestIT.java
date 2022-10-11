80165215f749e2199d9c8ee83fe64418f15ea046
Quachett <quachett@gmail.com>
2022-10-12 00:05:38 +0200

    WEFWD

--- src/test/java/synapse/content/scribe/frontend/ScribeRestIT.java
+++
@@ -1,8 +0,0 @@
-package synapse.content.scribe.frontend;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-public class ScribeRestIT extends ScribeRestTest {
-    // Execute the same tests but in packaged mode.
-}
